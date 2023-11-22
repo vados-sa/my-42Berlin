6435e3a92c6a066ba04e1142f6c4d984733284b9
Vanessa Dos Santos <vados-sa@c4a7c3.42berlin.de>
2023-11-22 09:42:24 +0100

    modified ft_strlen prototype

--- my-libft/ft_strlen.c
+++ my-libft/ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:30:50 by vados-sa          #+#    #+#             */
-/*   Updated: 2023/11/14 14:41:50 by vados-sa         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:41:44 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include <stdio.h>
-#include <string.h> */
+#include <stdio.h>
+#include <string.h>
 
-int	ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 
-int	ft_strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (str[i] != '\0')
