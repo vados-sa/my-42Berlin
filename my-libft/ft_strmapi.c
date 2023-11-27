35b1c1449a05d564045fbfaa54b1f9b4ebb608f1
Vanessa dos Santos <vanessasantos@Vanessas-MBP.fritz.box>
2023-11-28 00:29:13 +0100

    ft_strmapi

---
+++ my-libft/ft_strmapi.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_str;
+	size_t	i;
+
+	new_str = malloc((1 + ft_strlen(s)) * sizeof(char));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	new_str[i] = 0;
+	return (new_str);
+}
