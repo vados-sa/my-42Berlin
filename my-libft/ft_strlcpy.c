57e41dd43b525a4b4a8697c4c6411f182230223d
Vanessa Dos Santos <vados-sa@c0c1c3.42berlin.de>
2023-11-30 15:41:56 +0100

    strlcpy fixed

--- my-libft/ft_strlcpy.c
+++ my-libft/ft_strlcpy.c
@@ -6,22 +6,25 @@
 /*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:58:16 by vanessasant       #+#    #+#             */
-/*   Updated: 2023/11/30 10:42:53 by vados-sa         ###   ########.fr       */
+/*   Updated: 2023/11/30 15:41:08 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int	i;
+	size_t	i;
+	size_t	src_len;
 
+	src_len = ft_strlen(src);
 	i = 0;
-	while (i < (size - 1) && src[i] != '\0')
+	if (dstsize > src_len + 1)
+		ft_memcpy(dst, src, src_len + 1);
+	else if (dstsize != 0)
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = 0;
 	}
-	dst[i] = '\0';
-	return (i);
+	return (src_len);
 }
