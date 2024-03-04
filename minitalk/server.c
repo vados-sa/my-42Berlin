db9aa6b1119186bac21fe72712038f73768edc80
Vanessa Dos Santos <vados-sa@c4c6c1.42berlin.de>
2024-03-04 19:02:36 +0100

    started minitalk

---
+++ minitalk/server.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 19:00:35 by vados-sa          #+#    #+#             */
+/*   Updated: 2024/03/04 19:01:49 by vados-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+/* getpid: Returns the process ID (PID) of the calling process. 
+This is often used by the server to print its PID, which clients 
+need to communicate with it. */
