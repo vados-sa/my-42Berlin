9c20aac3660d3a37d164d66dd9757ddb227cf81c
Vanessa Dos Santos <vados-sa@c0c5c3.42berlin.de>
2024-03-05 16:28:27 +0100

    organized a little

--- ft_printf/utils.c
+++ ft_printf/utils.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/19 10:11:06 by vados-sa          #+#    #+#             */
-/*   Updated: 2023/12/20 12:35:24 by vados-sa         ###   ########.fr       */
+/*   Created: 2024/03/05 16:16:29 by vados-sa          #+#    #+#             */
+/*   Updated: 2024/03/05 16:27:06 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../ft_printf.h"
+#include "ft_printf.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (i);
+	while (str[i])
+		i++;
+	return (i);
+}
 
 int	ft_putnbr_base(int nbr, char *base)
 {
@@ -37,6 +49,43 @@ int	ft_putnbr_base(int nbr, char *base)
 		}
 		if (i < 0)
 			return (-1);
+	}
+	return (i);
+}
+
+int	ft_putnbr_base_unsign(unsigned int nbr, char *base)
+{
+	long int	len;
+	int			i;
+
+	len = ft_strlen(base);
+	i = 0;
+	if (base)
+	{
+		if (nbr < len)
+			i += print_char(base[nbr]);
+		if (nbr >= len)
+		{
+			i += ft_putnbr_base_unsign(nbr / len, base);
+			i += ft_putnbr_base_unsign(nbr % len, base);
+		}
+		if (i < 0)
+			return (-1);
+	}
+	return (i);
+}
+
+int	ft_putnbr_hex_base(unsigned long long nbr, char *base)
+{
+	int				i;
+
+	i = 0;
+	if (nbr <= 15)
+		i += print_char(base[nbr]);
+	if (nbr > 15)
+	{
+		i += ft_putnbr_hex_base(nbr / 16, base);
+		i += ft_putnbr_hex_base(nbr % 16, base);
 	}
 	return (i);
 }
