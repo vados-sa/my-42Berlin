10cec53119317a4d1c4c02fb3630db9ae77a90b1
Vanessa dos Santos <vanessasantos@Vanessas-MBP.fritz.box>
2024-03-06 09:45:12 +0100

    moved into a folder

---
+++ push_swap/push_sawp/init_node_struct.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_node_struct.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 13:59:40 by vados-sa          #+#    #+#             */
+/*   Updated: 2024/03/03 14:25:58 by vados-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	set_target_for_a(t_stack *a, t_stack *b);
+static void	set_target_for_b(t_stack *b, t_stack *a);
+static void	push_cost_analysis(t_stack *stack, t_stack *target_stack);
+
+void	init_nodes_a(t_stack **a, t_stack **b)
+{
+	find_index(*a);
+	find_index(*b);
+	set_target_for_a(*a, *b);
+	push_cost_analysis(*a, *b);
+	set_cheapest(*a);
+}
+
+void	init_nodes_b(t_stack **b, t_stack **a)
+{
+	find_index(*b);
+	find_index(*a);
+	set_target_for_b(*b, *a);
+}
+
+/* For each node in A, this function interates through the nodes in B to find
+the 'target_node'. The 'target_node' should be the the biggest node in B that
+is still smaller than the node in A ("closest/biggest smaller").
+'match_value' keeps track of the closest number to the node in A, so that the
+next possible smaller nodes in B, with values < 'match_value' are not 
+targeted. */
+static void	set_target_for_a(t_stack *a, t_stack *b)
+{
+	t_node	*node_a;
+	t_node	*node_b;
+	t_node	*target_node;
+	long	match_value;
+
+	node_a = a->top;
+	while (node_a)
+	{
+		node_b = b->top;
+		match_value = LONG_MIN;
+		while (node_b)
+		{
+			if (node_a->value > node_b->value && node_b->value > match_value)
+			{
+				match_value = node_b->value;
+				target_node = node_b;
+			}
+			node_b = node_b->next;
+		}
+		if (match_value == LONG_MIN)
+			node_a->target_node = find_max(b);
+		else
+			node_a->target_node = target_node;
+		node_a = node_a->next;
+	}
+}
+
+static void	set_target_for_b(t_stack *b, t_stack *a)
+{
+	t_node	*node_b;
+	t_node	*node_a;
+	t_node	*target_node;
+	long	match_value;
+
+	node_b = b->top;
+	node_a = a->top;
+	match_value = LONG_MAX;
+	while (node_a)
+	{
+		if (node_b->value < node_a->value && node_a->value < match_value)
+		{
+			match_value = node_a->value;
+			target_node = node_a;
+		}
+		node_a = node_a->next;
+	}
+	if (match_value == LONG_MAX)
+		node_b->target_node = find_min(a);
+	else
+		node_b->target_node = target_node;
+}
+
+static void	push_cost_analysis(t_stack *stack, t_stack *target_stack)
+{
+	t_node	*current;
+	int		len_a;
+	int		len_b;
+
+	current = stack->top;
+	len_a = stack_len(stack);
+	len_b = stack_len(target_stack);
+	while (current)
+	{
+		current->push_price = current->index;
+		if (!current->above_median)
+			current->push_price = len_a - current->index;
+		if (current->target_node->above_median == true)
+			current->push_price += current->target_node->index;
+		else
+			current->push_price += len_b - current->target_node->index;
+		current = current->next;
+	}
+}
