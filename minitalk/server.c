a5458c2f900bb062f92327444ab2716ef325c138
Vanessa Dos Santos <vados-sa@c0c7c1.42berlin.de>
2024-03-12 10:30:54 +0100

    cleanup

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,16 +6,14 @@
 /*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 19:00:35 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/03/09 17:26:45 by vados-sa         ###   ########.fr       */
+/*   Updated: 2024/03/12 09:48:55 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/* void    handle_signal(int signum, siginfo_t *info, void *context)
+void    handle_signal(int sig)
 {
-    (void)info; // Unused parameter
-    (void)context; // Unused parameter
 	if (signal == SIGUSR1)
 	{
 		ft_printf("Received SIGUSR1\n");
@@ -26,13 +24,13 @@
 		ft_printf("Received SIGUSR2\n");
 		// do something
 	}
-} */
+}
 
 int	main()
 {
 	ft_printf("Server PID: %d\n", getpid());
-	//signal(SIGUSR1, handle_signal);
-	//signal(SIGUSR2, handle_signal);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	while (1)
 	{
 		pause(); // Wait for signals
