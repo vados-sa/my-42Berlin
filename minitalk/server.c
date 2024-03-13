91ee94ee643412951f70e93d831b6729a29b9a32
Vanessa dos Santos <vanessasantos@Vanessas-MBP.fritz.box>
2024-03-13 14:26:49 +0100

    server now saves the characters in a buffer; server can only handle 1023 bytes long messages (1024th is for '\0')

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: vanessasantos <vanessasantos@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 19:00:35 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/03/13 12:05:22 by vanessasant      ###   ########.fr       */
+/*   Updated: 2024/03/13 13:39:24 by vanessasant      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "./ft_printf/ft_printf.h"
 # include "./libft/libft.h"
 
-static void    handle_signal(int sig)
+/* static void    handle_signal(int sig)
 {
 	static unsigned char	byte;
 	static int				bit_count;
@@ -33,6 +33,36 @@ static void    handle_signal(int sig)
 			return ;
 		}
 		ft_printf("%c", byte); //print each byte until the message it's over.
+        byte = 0;
+		bit_count = 0;
+	}
+} */
+
+#define MAX_MESSAGE_LENGTH 1024
+
+static void    handle_signal(int sig)
+{
+	static unsigned char	byte;
+	static int				bit_count;
+	static char message[MAX_MESSAGE_LENGTH];
+    static int message_length;
+
+	if (sig == SIGUSR1) // bit 1;
+		byte |= (1 << (7 - bit_count));
+	bit_count++;
+	if (bit_count == 8)
+	{
+		if (byte == 4)
+		{
+			ft_printf("%s\n", message);
+            ft_memset(message, 0, MAX_MESSAGE_LENGTH);
+            message_length = 0;
+		}
+		else
+		{
+            if (message_length < MAX_MESSAGE_LENGTH - 1)
+                message[message_length++] = byte;
+        }
         byte = 0;
 		bit_count = 0;
 	}
