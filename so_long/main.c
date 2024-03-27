1937b0bce3d9f241e24209ba297b70458a0832d7
Vanessa Dos Santos <vados-sa@c0c7c1.42berlin.de>
2024-03-27 11:28:02 +0100

    organized main a little

--- so_long/main.c
+++ so_long/main.c
@@ -12,21 +12,21 @@ int	close_window(t_wrapper *wrapper)
 	return (0);
 }
 
-int	render(t_data *data)
+int	render(t_game *game)
 {
 	/* if window has been destroyed, we don't want to put the pixel ! */
-	if (data->win_ptr)
-		mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
+	if (game->win_ptr)
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr, 
 			WIN_WIDTH / 2, WIN_HEIGHT / 2, MAG_PIXEL);
 	return (0);
 }
 
-int	render_background(t_data *data)
+int	render_background(t_game *game)
 {
 	int	x;
 	int	y;
 
-	if (!data->win_ptr)
+	if (!game->win_ptr)
 		return (1);
 	y = 0;
 	while (y <= WIN_HEIGHT)
@@ -34,7 +34,7 @@ int	render_background(t_data *data)
 		x = 0;
 		while (x <= WIN_WIDTH)
 		{
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, MAG_PIXEL);
+			mlx_pixel_put(game->mlx_ptr, game->win_ptr, x, y, MAG_PIXEL);
 			x++;
 		}
 		y++;
@@ -42,29 +42,46 @@ int	render_background(t_data *data)
 	return (0);
 }
 
+static int	init_game(t_game *game)
+{
+	game->mlx_ptr = mlx_init();
+	if (!game->mlx_ptr)
+	{	
+		ft_printf("Failed to initialize mlx.\n");
+		return (1);
+	}
+	game->win_ptr = mlx_new_window(game->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, \
+				"Barbie Dream World");
+	if (!game->win_ptr)
+	{
+		free(game->win_ptr);
+		ft_printf("Failed to create a window.\n");
+        return 1;
+	}
+	return (0);
+}
+
+static void	setup_hooks(t_game *game, t_wrapper *wrapper)
+{
+	mlx_loop_hook(game->mlx_ptr, &render_background, game);
+	mlx_hook(game->win_ptr, KeyPress, KeyPressMask, &handle_keypress, wrapper);
+	mlx_hook(game->win_ptr, 17, 1L<<17, &close_window, wrapper);
+}
+
 int	main(int ac, char *av[])
 {
-	t_data		data;
+	t_game		game;
 	t_map		*map;
 	t_wrapper	wrapper;
 
 	check_arguments(ac);
 	map = read_map(av[1]);
-	wrapper.data = &data;
+	if (init_game(&game))
+		error_exit(map, -2);
+	wrapper.game = &game;
 	wrapper.map = map;
-	data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
-		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Game");
-	if (data.win_ptr == NULL)
-	{
-		free(data.win_ptr);
-		return (MLX_ERROR);
-	} /* Setup hooks */ 
-	mlx_loop_hook(data.mlx_ptr, &render_background, &data);
-	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &wrapper);
-	mlx_hook(data.win_ptr, 17, 1L<<17, close_window, &wrapper);
-	mlx_loop(data.mlx_ptr);
+	setup_hooks(&game, &wrapper);
+	mlx_loop(game.mlx_ptr); // start the game loop
 	//cleanup(&wrapper); // seems to be unecessary
 	return (0);
 }
