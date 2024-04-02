b341825f21bf7cc8b296f6276d41bc1025edfd5d
Vanessa Dos Santos <vados-sa@c4a9c1.42berlin.de>
2024-04-02 19:59:26 +0200

    norminette

--- so_long/error_management.c
+++ so_long/error_management.c
@@ -1,91 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_management.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 19:59:13 by vados-sa          #+#    #+#             */
+/*   Updated: 2024/04/02 19:59:14 by vados-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
-
-static void	free_collectibles_1(t_game *game)
-{
-	if (game->key_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->key_img);
-		game->key_img = NULL;
-	}
-	if (game->iphone_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->iphone_img);
-		game->iphone_img = NULL;
-	}
-	if (game->purse_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->purse_img);
-		game->purse_img = NULL;
-	}
-	if (game->lipstick_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->lipstick_img);
-		game->lipstick_img = NULL;
-	}
-}
-
-static void	free_collectibles_2(t_game *game)
-{
-	if (game->wallet_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->wallet_img);
-		game->wallet_img = NULL;
-	}
-	if (game->hphones_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->hphones_img);
-		game->hphones_img = NULL;
-	}
-	if (game->skates_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->skates_img);
-		game->skates_img = NULL;
-	}
-	if (game->dog_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->dog_img);
-		game->dog_img = NULL;
-	}
-}
 
 void	clean_game(t_game *game)
 {
-	if (game->floor_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->floor_img);
-		game->floor_img = NULL;
-	}
-	if (game->wall_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->wall_img);
-		game->wall_img = NULL;
-	}
-	if (game->house_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->house_img);
-		game->house_img = NULL;
-	}
-	if (game->exit_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->exit_img);
-		game->exit_img = NULL;
-	}
-	if (game->barbie_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->barbie_img);
-		game->barbie_img = NULL;
-
-	}
-	if (game->exit2_img)
-	{
-		mlx_destroy_image(game->mlx_ptr, game->exit2_img);
-		game->exit2_img = NULL;
-
-	}
+	free_extra_sprites(game);
+	free_images(game);
 	free_collectibles_1(game);
 	free_collectibles_2(game);
 	free_bonus(game);
 	free_bonus2(game);
+}
+
+void	free_map(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->height)
+		free(map->tiles[i++]);
+	free(map->tiles);
+	free(map);
+	return ;
 }
 
 void	cleanup(t_wrapper *wrapper)
@@ -125,16 +71,4 @@ void	error_exit(t_map *map, int fd)
 		ft_printf("Please insert a valid map.\n\n");
 	ft_printf("\nError!\n\n");
 	exit (1);
-}
-
-void	free_map(t_map *map)
-{
-	int	i;
-
-	i = 0;
-	while (i < map->height)
-		free(map->tiles[i++]);
-	free(map->tiles);
-	free(map);
-	return ;
 }
